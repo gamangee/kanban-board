76b075aec81cdf6254f9cac0294d8b128c4d804d
gamangee <gamangee1991@gmail.com>
2023-03-01 16:37:14 +0900

    multi-boards

--- src/KanbanBoard.tsx
+++ src/KanbanBoard.tsx
@@ -1,58 +1,38 @@
 import React from 'react';
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { toDoState } from './atoms';
+import Board from './components/Board';
+import DragabbleCard from './components/DragabbleCard';
 
 export default function KanbanBoard() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     if (!destination) return;
-    setToDos((oldToDos) => {
-      const toDosCopy = [...oldToDos];
-      // 1) Delete item on source.index
-      console.log('Delete item on', source.index);
-      console.log(toDosCopy);
-      toDosCopy.splice(source.index, 1);
-      console.log('Deleted item');
-      console.log(toDosCopy);
-      // 2) Put back the item on the destination.index
-      console.log('Put back', draggableId, 'on ', destination.index);
-      toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
-      return toDosCopy;
-    });
+    // setToDos((oldToDos) => {
+    //   const toDosCopy = [...oldToDos];
+    //   // 1) Delete item on source.index
+    //   console.log('Delete item on', source.index);
+    //   console.log(toDosCopy);
+    //   toDosCopy.splice(source.index, 1);
+    //   console.log('Deleted item');
+    //   console.log(toDosCopy);
+    //   // 2) Put back the item on the destination.index
+    //   console.log('Put back', draggableId, 'on ', destination.index);
+    //   toDosCopy.splice(destination?.index, 0, draggableId);
+    //   console.log(toDosCopy);
+    //   return toDosCopy;
+    // });
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId='one'>
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card
-                        ref={magic.innerRef}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                      >
-                        {toDo}
-                      </Card>
-                    )}
-                  </Draggable>
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
@@ -61,7 +41,7 @@ export default function KanbanBoard() {
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 600px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -72,20 +52,6 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
-`;
-
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
 `;
---
+++ src/components/Board.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+import DragabbleCard from './DragabbleCard';
+
+interface IBoardProps {
+  toDos: string[];
+  boardId: string;
+}
+
+export default function Board({ toDos, boardId }: IBoardProps) {
+  return (
+    <Droppable droppableId={boardId}>
+      {(magic) => (
+        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
+          {toDos.map((toDo, index) => (
+            <DragabbleCard key={toDo} index={index} toDo={toDo} />
+          ))}
+          {magic.placeholder}
+        </Wrapper>
+      )}
+    </Droppable>
+  );
+}
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
